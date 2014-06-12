ba5bcedbfe396b43c38f610a2a75d1215265bd73
JoinTheRealms <jsh.janssen@gmail.com>
2014-06-12 17:42:29 +1200

    12/06/2014 - minor jquery changes

--- script/buttons.js
+++ script/buttons.js
@@ -107,12 +107,22 @@ $(document).ready(function(){
 
 $(document).ready(function(){
 	$('#leftNavButton1').mouseover(function(){
-		$('#leftNavButton1').css({'width': '115px', 'background': 'linear-gradient(to bottom, #797979 1%,#333333 100%'});
-		$('#leftNavButton1 > .subLink').css({'font-size': '18px'});
+		if( $(".navLink").css("font-size") == '20px') {
+			$('#leftNavButton1').css({'width': '115px', 'background': 'linear-gradient(to bottom, #797979 1%,#333333 100%'});
+			$('#leftNavButton1 > .subLink').css({'font-size': '23px'});
+		} else {
+			$('#leftNavButton1').css({'width': '115px', 'background': 'linear-gradient(to bottom, #797979 1%,#333333 100%'});
+			$('#leftNavButton1 > .subLink').css({'font-size': '12px'});
+		};
 	});
 	$('#leftNavButton1').mouseout(function(){
-		$('#leftNavButton1').css({'width': '110px', 'background': 'linear-gradient(to bottom, #6b6b6b 1%,#333333 100%'});
-		$('#leftNavButton1 > .subLink').css({'font-size': '15px'});
+		if( $(".subLink").css("font-size") > "15px") {
+			$('#leftNavButton1').css({'width': '110px', 'background': 'linear-gradient(to bottom, #6b6b6b 1%,#333333 100%'});
+			$('#leftNavButton1 > .subLink').css({'font-size': '23px'});
+		} else {
+			$('#leftNavButton1').css({'width': '115px', 'background': 'linear-gradient(to bottom, #797979 1%,#333333 100%'});
+			$('#leftNavButton1 > .subLink').css({'font-size': '12px'});
+		};
 	});
 });
 
